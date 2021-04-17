e98692b1cb97597ed841f6b3cc92c29238e6c2ad
Turkish Lejen <76731270+lejencodes@users.noreply.github.com>
2021-04-17 11:52:56 +0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://the-lejen.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://lejencodes.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
